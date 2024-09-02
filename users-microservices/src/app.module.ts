a2d09b5179821f527c62da73a96ab26009cbab49
Emmanuel Ugwueze <emmanuelugwueze6@gmail.com>
2024-09-02 11:37:12 +0100

    changes

--- users-microservices/src/app.module.ts
+++ users-microservices/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserMicroserviceController } from './user/controllers/user/user.controller';
 import { UserModule } from './user/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'mysql',
+      port: 5432,
+      username: 'postgres',
+      password: 'ami',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController, UserMicroserviceController],
   providers: [AppService],
 })
--- users-microservices/src/main.ts
+++ users-microservices/src/main.ts
@@ -3,9 +3,11 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  //for creating microservice
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
+      ///we specify the transport protocol and the connection options as NATS
       transport: Transport.NATS,
       options: {
         servers: ['nats://nats'],
